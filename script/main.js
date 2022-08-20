b9a876db45511292f72cb620776cfab9afa7d4a7
kimbg <rlaqudrhks6@gmail.com>
2022-08-20 17:37:29 +0900

    top,bottom navbar complete

---
+++ script/main.js
@@ -0,0 +1,55 @@
+$(function() {    
+
+    $("#itemContainer a").hover(function() {
+        let id = $(this).attr('id');
+
+        if(id == 1) {
+            $(this).children('img').attr('src','img/bank2.png');
+            $(this).children('span').css('color','rgb(251,159,182)');
+        } else if(id == 2) {
+            $(this).children('img').attr('src','img/restaurant2.png');
+            $(this).children('span').css('color','rgb(251,159,182)');
+        } else if(id == 3) {
+            $(this).children('img').attr('src','img/eiffel-tower2.png');
+            $(this).children('span').css('color','rgb(251,159,182)');
+        }else if(id == 4) {
+            $(this).children('img').attr('src','img/information2.png');
+            $(this).children('span').css('color','rgb(251,159,182)');
+        }
+    },function() {
+        let id = $(this).attr('id');
+
+        if(id == 1) {
+            $(this).children('img').attr('src','img/bank.png');
+            $(this).children('span').css('color','black');
+        } else if(id == 2) {
+            $(this).children('img').attr('src','img/restaurant.png');
+            $(this).children('span').css('color','black');
+        } else if(id == 3) {
+            $(this).children('img').attr('src','img/eiffel-tower.png');
+            $(this).children('span').css('color','black');
+        }else if(id == 4) {
+            $(this).children('img').attr('src','img/information.png');
+            $(this).children('span').css('color','black');
+        }
+    })
+
+    $("#itemContainer a").click(function() {
+        $("#itemContainer a").css('border-bottom','none');        
+        $(this).css('border-bottom','2px solid black');
+    })
+
+
+
+    $(window).resize(function() {
+        let width = $(window).innerWidth();
+        
+        if(width < 800) {
+            $("#topNavbar2").removeClass('top');
+            $("#topNavbar2").addClass('bottom');
+        } else {
+            $("#topNavbar2").addClass('top');
+            $("#topNavbar2").removeClass('bottom');
+        }
+    })
+})
